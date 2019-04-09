4b3e1c49f2b2e0af2774e4ee276317fcca807bc2
ntropy-esa <38524935+ntropy-esa@users.noreply.github.com>
2019-04-09 23:58:12 +0200

    Add files via upload

---
+++ calendar.js
@@ -0,0 +1,7 @@
+var calendar = [
+{ eventName: 'Workshop at Symbios', calendar: 'Workshop', color: 'orange', date: moment("2019-04-07", "YYYY-MM-DD"), url:"link to FB event"},
+{ eventName: 'Workshop at KTH', calendar: 'Workshop', color: 'orange', date: moment("2019-04-25", "YYYY-MM-DD"), url:"link to FB event"},
+{ eventName: 'Monthly meetup', calendar: 'Meetup', color: 'blue', date: moment("2019-04-15", "YYYY-MM-DD"), url:"link to FB event"},
+{ eventName: 'Workshop at KTH', calendar: 'Workshop', color: 'orange', date: moment("2019-04-19", "YYYY-MM-DD"), url:"link to FB event"},
+{ eventName: 'Monthly meetup', calendar: 'Meetup', color: 'blue', date: moment("2019-05-10", "YYYY-MM-DD"), url:"link to FB event"},
+];
