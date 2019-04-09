ac63f9ef6ad00f9ba3a538ac48be9738f3fb3526
ntropy-esa <38524935+ntropy-esa@users.noreply.github.com>
2019-04-10 00:20:09 +0200

    Update calendar.js

--- calendar.js
+++ calendar.js
@@ -4,5 +4,5 @@ var calendar = [
 { eventName: 'Monthly meetup', calendar: 'Meetup', color: 'blue', date: moment("2019-04-15", "YYYY-MM-DD"), url:"link to FB event"},
 { eventName: 'Workshop at KTH', calendar: 'Workshop', color: 'orange', date: moment("2019-04-19", "YYYY-MM-DD"), url:"link to FB event"},
 { eventName: 'Monthly meetup', calendar: 'Meetup', color: 'blue', date: moment("2019-05-10", "YYYY-MM-DD"), url:"link to FB event"},
-{ eventName: 'Push', calendar: 'Meetup', color: 'blue', date: moment("2019-05-15", "YYYY-MM-DD"), url:"link to FB event"},
+{ eventName: 'Push', calendar: 'Meetup', color: 'red', date: moment("2019-05-15", "YYYY-MM-DD"), url:"link to FB event"},
 ];
