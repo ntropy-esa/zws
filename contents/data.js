52dc76ed69cc49e3533d8ab0322d1f29c659ca54
ntropy-esa <38524935+ntropy-esa@users.noreply.github.com>
2019-08-11 23:11:51 +0200

    Add files via upload

---
+++ contents/data.js
@@ -0,0 +1,86 @@
+// PROJECTS DATA
+	// [ ] a list to retrive with [interger]
+	// {"name"} to retrive with .name 
+var projects = [
+	{"EN":{"title":"Bring Your Own - BYO",
+		   "description":"The BYO group spots zero-waste friendly shops in Stockholm area and promotes their engagement. With the distribution of labels, we make it easy for you to find these shops!",
+		   "more":""
+		  },
+	 "SE":{"title":"Bring Your Own - BYO",
+		   "description":"The BYO group selects zero-waste friendly shops in Stockholm and promotes their engagement. With the distribution a distinctive label and the map below, we make it easy for you to find these shops!",
+		   "more":""
+		},
+	"src":"img/bg-showcase-2.jpg",
+	"widget":"map",
+	 "images":"",
+	 "lastupdate":"2019-02-10",
+	 "participants":[],
+	 "links":[{"href":"","name":""}],
+	 "documents":[{"href":"","name":""},]
+	},
+	{"EN":{"title":"Workshops", 
+	"description":"Step by step, the workshop group will guide you on your zero-waste journey by arranging DYI tutorials and courses on different topics.",
+	"more":""
+	},
+	 "SE":{"title":"Workshops",
+	 "description":"Step by step, the workshop group will guide you on your zero-waste journey by arranging DYI tutorials and courses on different topics.",
+	 "more":""
+	},
+	 "src":"img/bg-showcase-4.jpg",
+	 "widget":"",
+	 "images":"",
+	 "lastupdate":"2019-03-02",
+	 "participants":[
+		{"name":"", "affiliation":""},
+		],
+	 "links":[{"href":"","name":""}],
+	 "documents":[{"href":"","name":""}]
+	},
+	{"EN":{"title":"Meetups",
+	"description":"Interested in meeting like-minded people or curious to learn about this thing we call Zero Waste? The meetups are the perfect opportunity to meet informally with the community. A meetup can take different forms depending on the mood of the moment: fika, presentation, movie screening, picnic etc",
+	"more":""},
+	 "SE":{"title":"Meetups",
+	"description":"Interested in meeting like-minded people or curious to learn about this thing we call Zero Waste? The meetups are the perfect opportunity to meet informally with the community. A meetup can take different forms depending on the mood of the moment: fika, presentation, movie screening, picnic etc",
+	"more":""},
+	 "src":"img/bg-showcase-4.jpg",
+	 "widget":"cal",
+	 "images":"",
+	 "lastupdate":"2019-03-02",
+	 "participants":[
+		{"name":"", "affiliation":""},
+		],
+	 "links":[{"href":"","name":""}],
+	 "documents":[{"href":"","name":""}]
+	},
+	{"EN":{"title":"Research and strategy",
+	"description":"This group compiles statistics and facts on Stockholm's waste situation. It also develops a strategy for Stockholm that differs from the current municipal plans.",
+	"more":""},
+	 "SE":{"title":"Research and strategy",
+	"description":"This group compiles statistics and facts on Stockholm's waste situation. It also develops a strategy for Stockholm that differs from the current municipal plans.",
+	"more":""},
+	 "src":"img/bg-showcase-5.jpg",
+	 "widget":"",
+	 "images":"",
+	 "lastupdate":"2019-03-02",
+	 "participants":[
+		{"name":"", "affiliation":""},
+		],
+	 "links":[{"href":"","name":""}],
+	 "documents":[{"href":"","name":""}]
+	}];
+	
+// BASE CONTENT DATA
+var base = {
+	"EN":{"vision_tit":"Mission and principles",
+		  "vision_txt":'Our mission is to inspire Stockholmers to reduce their ecological footprint and the amount of waste they generate by promoting the six principles:',
+		  "moto_tit":"Let's make trash a thing of the past!",
+		  "sixRs":'<img src="img/6r.png" alt="Six R\'s" class="sixRs" />',
+		  "more":""},
+	"SE":{"vision_tit":"Vision, mission och mål",
+		  "vision_txt":"Translation coming soon...",
+		  "team_part_tit":"Projekt deltagare",
+		  "moto_tit":"Skapa en avfallsfritt Stockholm",                    
+		  "sixRs":'<img src="img/6r.png" alt="Six R\'s" class="sixRs" />',
+		  "more":""
+		  }
+};
---
+++ contents/toolkit.js
@@ -0,0 +1,33 @@
+var myDivaExport =[	
+	{"title":"ZW Christmas - Sticker",
+	"language":"en",
+	"author":[{"family":"ZWS","given":""}],
+	"abstract":"Compilation of all the tips we shared during the Christmas Meetup, in December 2018.",
+	"published":[{"raw":"2019-05-01T17:25:04.217+02:00"}],
+	"URL":"https://drive.google.com/open?id=1yBGFlcKrZoS1fkrH60I3g3J-kt9k2VgZ"},
+
+	{"title":"Bring Your Own - Flyer for shop owners",
+	"language":"en",
+	"author":[{"family":"ZWS","given":""}],
+	"abstract":"General instructions for our Bring Your Own campaing, specially written for shop owners.",
+	"published":[{"raw":"2019-07-10T17:25:04.217+02:00"}],
+	"URL":"https://drive.google.com/file/d/1Z3e0odXdHBBKEFTUdB7Yf7GbHxiqHAur"}	,
+
+	{"title":"Bring Your Own - Flyer for ambassadors",
+	"language":"en",
+	"author":[{"family":"ZWS","given":""}],
+	"abstract":"General instructions for our Bring Your Own campaing, specially written for ambassadors.",
+	"published":[{"raw":"2019-07-10T17:25:04.217+02:00"}],
+	"URL":"https://drive.google.com/file/d/1gJlsPcH0R7_d6QeizM6FBQz_O7S6KbZa"}	,
+
+	{"title":"Bring Your Own - Project presentation",
+	"language":"en",
+	"author":[{"family":"ZWS","given":""}],
+	"abstract":"A short presentation of the Bring Your Own campaing.",
+	"published":[{"raw":"2019-07-10T17:25:04.217+02:00"}],
+	"URL":"https://drive.google.com/file/d/1Qe6C47Rydl07l4L3_95rS1jJWKIT3S-_"}	
+	
+	]
+    
+    
+    
