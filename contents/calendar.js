630029859f0a6244726794f47c9e174772fdebda
ntropy-esa <38524935+ntropy-esa@users.noreply.github.com>
2019-08-11 23:07:16 +0200

    Update and rename ci_calendar.js to calendar.js

---
+++ contents/calendar.js
@@ -0,0 +1,8 @@
+var calendar = [
+{ eventName: 'Workshop at Symbios', calendar: 'Workshop', color: 'orange', date: moment("2019-04-07", "YYYY-MM-DD"), url:"#"},
+{ eventName: 'Meetup: easy swaps to reduce waste.', calendar: 'Meetup', color: 'blue', date: moment("2019-05-06", "YYYY-MM-DD"), url:"https://www.facebook.com/events/353714908591085/"},
+{ eventName: 'Meetup: ZW festivities (midsummer and more).', calendar: 'Meetup', color: 'blue', date: moment("2019-06-02", "YYYY-MM-DD"), url:"#"},
+{ eventName: 'Workshop - ZW for beginners', calendar: 'Workshop', color: 'blue', date: moment("2019-06-04", "YYYY-MM-DD"), url:"https://www.studieframjandet.se/stockholms-lan/gustav-arrangemang/kalenderhandelser/2019/juni/zero-waste-workshop-for-nyborjare/"},
+{ eventName: 'Meetup: ZW travelling. How to travel with low impact?', calendar: 'Meetup', color: 'blue', date: moment("2019-07-07", "YYYY-MM-DD"), url:"#"},  
+{ eventName: 'Clothes Sawp', calendar: 'Swap', color: 'red', date: moment("2019-08-10", "YYYY-MM-DD"), url:"https://www.facebook.com/events/1349128968598806/"},
+];
--- contents/ci_calendar.js
+++
@@ -1,8 +0,0 @@
-var calendar = [
-{ eventName: 'Workshop at Symbios', calendar: 'Workshop', color: 'orange', date: moment("2019-04-07", "YYYY-MM-DD"), url:"link to FB event"},
-{ eventName: 'Workshop at KTH', calendar: 'Workshop', color: 'orange', date: moment("2019-04-25", "YYYY-MM-DD"), url:"link to FB event"},
-{ eventName: 'Monthly meetup', calendar: 'Meetup', color: 'blue', date: moment("2019-04-15", "YYYY-MM-DD"), url:"link to FB event"},
-{ eventName: 'Workshop at KTH', calendar: 'Workshop', color: 'orange', date: moment("2019-04-19", "YYYY-MM-DD"), url:"link to FB event"},
-{ eventName: 'Monthly meetup', calendar: 'Meetup', color: 'blue', date: moment("2019-05-10", "YYYY-MM-DD"), url:"link to FB event"},
-{ eventName: 'Clothes Sawp', calendar: 'Swap', color: 'red', date: moment("2019-08-10", "YYYY-MM-DD"), url:"https://www.facebook.com/events/1349128968598806/"},
-];
