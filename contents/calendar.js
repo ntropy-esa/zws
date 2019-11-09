a2666d49c4475b87201c895ee5810ec2c5d7e292
ntropy-esa <38524935+ntropy-esa@users.noreply.github.com>
2019-11-09 12:15:59 +0100

    Update calendar.js

--- contents/calendar.js
+++ contents/calendar.js
@@ -5,4 +5,7 @@ var calendar = [
 { eventName: 'Workshop - ZW for beginners', calendar: 'Workshop', color: 'blue', date: moment("2019-06-04", "YYYY-MM-DD"), url:"https://www.studieframjandet.se/stockholms-lan/gustav-arrangemang/kalenderhandelser/2019/juni/zero-waste-workshop-for-nyborjare/"},
 { eventName: 'Meetup: ZW travelling. How to travel with low impact?', calendar: 'Meetup', color: 'blue', date: moment("2019-07-07", "YYYY-MM-DD"), url:"#"},  
 { eventName: 'Clothes Swap', calendar: 'Swap', color: 'red', date: moment("2019-08-10", "YYYY-MM-DD"), url:"https://www.facebook.com/events/1349128968598806/"},
+{ eventName: 'Meetup: Alexandra Davidsson', calendar: 'Meetup', color: 'blue', date: moment("2019-11-14", "YYYY-MM-DD"), url:"https://www.facebook.com/events/747080762383103/"},
+{ eventName: 'Pop-up in Hornstull', calendar: 'Event', color: 'red', date: moment("2019-11-25", "YYYY-MM-DD"), url:""},
+{ eventName: 'Clothe Sawp', calendar: 'Swap', color: 'red', date: moment("2019-11-30", "YYYY-MM-DD"), url:""},
 ];
