e6b0c9c244ef39fe3f5b0b57a940627d5ae7a23f
ntropy-esa <38524935+ntropy-esa@users.noreply.github.com>
2019-11-09 15:27:24 +0100

    Added event
    /Elias

--- contents/calendar.js
+++ contents/calendar.js
@@ -7,5 +7,5 @@ var calendar = [
 { eventName: 'Clothes Swap', calendar: 'Swap', color: 'red', date: moment("2019-08-10", "YYYY-MM-DD"), url:"https://www.facebook.com/events/1349128968598806/"},
 { eventName: 'Meetup: Alexandra Davidsson', calendar: 'Meetup', color: 'blue', date: moment("2019-11-14", "YYYY-MM-DD"), url:"https://www.facebook.com/events/747080762383103/"},
 { eventName: 'Pop-up in Hornstull', calendar: 'Event', color: 'red', date: moment("2019-11-25", "YYYY-MM-DD"), url:""},
-{ eventName: 'Clothe Sawp', calendar: 'Swap', color: 'blue', date: moment("2019-11-30", "YYYY-MM-DD"), url:""},
+{ eventName: 'Clothe Sawp', calendar: 'Swap', color: 'red', date: moment("2019-11-30", "YYYY-MM-DD"), url:""},
 ];
